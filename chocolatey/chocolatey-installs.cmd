983ae065ed4c3ea91ec54b69a38a8d89dcc3bde2
Laurent Berteau <laurent@berteau.name>
2017-01-06 16:27:02 +0100

    Introducing chocolatey initial install script

---
+++ chocolatey/chocolatey-installs.cmd
@@ -0,0 +1,10 @@
+choco install firefox -y
+choco install thunderbird -y
+choco install 7zip -y
+choco install notepadplusplus -y
+choco install virtualbox -y
+choco install libreoffice -y
+choco install rufus -y
+rem TODO
+rem vlc
+rem openvpn ?
